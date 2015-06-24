27ed10913ccff246042ef750dd3feb19c57a7920
Matthew <grasbergerm@gmail.com>
2015-06-24 18:32:15 -0400

    Fixed link grab
    Added a clause to append ‘.jpg’ to the imgur link if it’s not there

--- background_changer.py
+++ background_changer.py
@@ -4,27 +4,27 @@ import subprocess
 import random
 import os
 
-f = open('top_earth.jpg','wb')
-user_agent = ("Top of Earth Porn Grab Bot"
-              )
+f = open('top_image.jpg','wb')
+user_agent = ("image grab")
 r = praw.Reddit(user_agent=user_agent)
-thing_limit = 10
 user_name = "GrasbergerM"
-for submission in r.get_subreddit('earthporn').get_top(limit=5):
+for submission in r.get_subreddit('carporn').get_top(limit=5):
     link = submission.url
     if random.randint(0,5) == 3:
         break
-f.write(urllib.urlopen(link+".jpg").read())
+if link[len(link)-4:len(link):1] != ".jpg":
+    link += ".jpg"
+f.write(urllib.urlopen(link).read())
 f.close()
 
 SCRIPT = """/usr/bin/osascript<<END
 tell application "Finder"
-set desktop picture to POSIX file "%s"
+set desktop picture to "%s" as POSIX file
 end tell
 END"""
 
 def set_desktop_background(filename):
     subprocess.Popen(SCRIPT%filename, shell=True)
-set_desktop_background('/Users/grasbergerm/Desktop/top_earth.jpg')
+set_desktop_background('/Users/grasbergerm/Projects/Reddit Background Changer/top_image.jpg')
 process_name = "Dock"
 os.system('pkill '+ process_name)
